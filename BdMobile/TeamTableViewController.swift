a86724c1919ff6778db4b0f06d03150b9360f94e
CHAMPION EMILIEN p1808037 <emilien.champion@etu.univ-lyon1.fr>
2021-02-24 12:14:50 +0100

    feat: added viewcontroler

---
+++ BdMobile/TeamTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BdMobile
+//
+//  Created by Emilien Champion on 24/02/2021.
+//
+
+import UIKit
+
+class TeamTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+        
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+
+}
+
